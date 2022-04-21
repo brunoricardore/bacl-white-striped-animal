f1ae060e39a25778f0bc77310073e7a6dfd00a8a
Bruno Ricardo <brunoricardore@gmail.com>
2022-04-21 00:50:14 -0300

    Content projection for input error + input validator for doubles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   public formGroup!: FormGroup;
+
+  private $destroy = new Subject();
 
   constructor(
     private readonly formBuilder: FormBuilder
@@ -17,13 +21,38 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.listenPasswordChanges();
+  }
+
+  ngOnDestroy() {
+    this.$destroy.next();
+  }
+
+  private listenPasswordChanges(): void {
+    this.getFormControl('password').valueChanges
+      .pipe(
+        takeUntil(this.$destroy)
+      ).subscribe(val => {
+       this.getFormControl('password').setValue(val.replace(/[^0-9]/g, ''), {emitEvent: false})
+    });
+  }
+
+  private checkForDoubles(): ValidatorFn {
+    return (control) => {
+      return !/((.)\2+)+/.test(control.value) ? {noDoubles: true} : null
+    }
   }
 
   private createFormGroup(): void {
     this.formGroup = this.formBuilder.group({
       name: [null, [Validators.required]],
       email: [null, [Validators.required]],
-      password: [null, [Validators.required]],
+      password: [null, [
+        Validators.required,
+        Validators.min(184759),
+        Validators.max(856920),
+        this.checkForDoubles()
+      ]],
     });
   }
 
